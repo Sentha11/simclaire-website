ee484407de701106a1f97751e6f2ed94063d0d01
Sentha11 <55805362+Sentha11@users.noreply.github.com>
2025-11-26 01:09:56 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -440,5 +440,5 @@ app.post("/webhook/whatsapp", async (req, res) => {
 // -----------------------------
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(🔥 Backend running on port ${PORT});
+  console.log(`🔥 Backend running on port ${PORT}`);
 });
