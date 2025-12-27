ed4018e13e7cbc42b61417e75917a89c4dcbda52
Sentha Saravanabavan <sendu319@hotmail.com>
2025-12-26 21:44:10 -0500

    Final2.8

--- backend/server.js
+++ backend/server.js
@@ -326,7 +326,7 @@ if (stripe && process.env.STRIPE_WEBHOOK_SECRET) {
               {
                 sku: metadata.productSku,
                 quantity: Number(metadata.quantity || 1),
-                type: "holiday_esim_api",
+                type: "holiday_esim",
               },
             ],
           };
